eaa12fcf7664306907d39482cbd4034a01139f1c
anthole95 <an.tho.le95@gmail.com>
2026-05-10 01:35:33 -0600

    Fix db.create_all to run on Gunicorn startup

--- app.py
+++ app.py
@@ -284,8 +284,9 @@ def booking():
 
     return render_template('booking.html', services=services)
 
-# ── Create tables and run ──
+# Create tables on startup
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
